137b52c9bc413466f56c2b8b9dfb6f9ee2937c2c
eliahusatat <eliahusatat@gmail.com>
2018-11-27 11:53:37 +0200

    adding test- not working for coords

---
+++ tests/Coords/MyCoordsTest.java
@@ -0,0 +1,50 @@
+package Coords;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MyCoordsTest {
+//
+//	@BeforeAll
+//	static void setUpBeforeClass() throws Exception {
+//	}
+//
+//	@AfterAll
+//	static void tearDownAfterClass() throws Exception {
+//	}
+//
+//	@BeforeEach
+//	void setUp() throws Exception {
+//	}
+//
+//	@AfterEach
+//	void tearDown() throws Exception {
+//	}
+
+	@Test
+	void testadd() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testdistance3d() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testvector3D() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testazimuth_elevation_dist() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testisValid_GPS_Point() {
+		fail("Not yet implemented");
+	}
+
+}
