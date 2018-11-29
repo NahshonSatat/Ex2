13eac871c57f9727872b4f953e920033ad93ca1a
unknown <nahashon.satat@gmail.com>
2018-11-29 19:10:55 +0200

    the algor

--- src/Algorithms/MultiCSV.java
+++ src/Algorithms/MultiCSV.java
@@ -1,5 +1,24 @@
 package Algorithms;
 
+import GIS.My_GIS_project;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 public class MultiCSV {
-
+	private My_GIS_project project;
+	String name_folder;
+	
+	public MultiCSV(String name) {
+		name_folder =name;
+		
+	}
+	public My_GIS_project Recursive() {
+		
+		return this.project;
+	}
+	public void toKml() {
+		
+	}
 }
