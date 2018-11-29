eff5a79604d3dd59845e03acb856c62cf930c7de
eliahusatat <eliahusatat@gmail.com>
2018-11-29 13:47:07 +0200

    meta data+gis element

--- src/GIS/My_GIS_element.java
+++ src/GIS/My_GIS_element.java
@@ -4,18 +4,24 @@ import Geom.Geom_element;
 import Geom.Point3D;
 
 public class My_GIS_element implements  GIS_element {
-Point3D P ;
-My_Meta_data DATA ; 
+    private Point3D Geom ;
+    private My_Meta_data Data ;
+    
+    
+    public My_GIS_element(String line) {
+    	
+    	
+    	
+    }
 	@Override
 	public Geom_element getGeom() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.Geom;
 	}
 
 	@Override
 	public Meta_data getData() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.Data;
 	}
 
 	@Override
--- src/GIS/My_Meta_data.java
+++ src/GIS/My_Meta_data.java
@@ -3,20 +3,25 @@ package GIS;
 import Geom.Point3D;
 
 public class My_Meta_data implements Meta_data {
-
+    private long UTC;
+    private String SSID;
+    private String MAC;
+    private String Type;
+    private String AccuracyMeters;   
+	
+	//
 	@Override
-	public long getUTC() {
-		return 0;
+	public long getUTC() { 	
+		return this.UTC;
 	}
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		String s="UTC :"+this.UTC+"SSID :"+this.SSID+"MAC :"+this.MAC+"Type :"+this.Type+"AccuracyMeters :"+this.AccuracyMeters;
+		return s;
 	}
 
 	@Override
 	public Point3D get_Orientation() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
