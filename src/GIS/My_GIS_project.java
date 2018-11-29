c0a5b6de939da3f15b66def1c5dde2da6e06002c
eliahusatat <eliahusatat@gmail.com>
2018-11-29 16:00:03 +0200

    meta data+gis element

--- src/GIS/My_GIS_project.java
+++ src/GIS/My_GIS_project.java
@@ -6,18 +6,6 @@ import java.util.HashSet;
 public class My_GIS_project  extends  HashSet<GIS_layer> implements GIS_project {
 
 	@Override
-	public boolean add(GIS_layer e) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean addAll(Collection<? extends GIS_layer> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
 	public Meta_data get_Meta_data() {
 		// TODO Auto-generated method stub
 		return null;
