3b8c712f42901667eaa756d522f53800cbceacaf
eliahusatat <eliahusatat@gmail.com>
2018-12-12 14:56:46 +0200

    fix gui

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -25,13 +25,12 @@ import Geom.Packman;
 
 
 public class MainWindow extends JFrame implements MouseListener
-
-
 {
 	public Game game;
 
 	public BufferedImage myImage;
 	
+	// the constructor 
 	public MainWindow() 
 	{
 		game=new Game();
@@ -42,14 +41,14 @@ public class MainWindow extends JFrame implements MouseListener
 	private void initGUI() 
 	{
 		MenuBar menuBar = new MenuBar();
-		Menu menu = new Menu("Menu"); 
-		MenuItem item1 = new MenuItem("menu item 1");
-		MenuItem item2 = new MenuItem("menu item 2");
-		Menu menu1 = new Menu("Menu1"); 
-		MenuItem item3 = new MenuItem("menu item 3");
-		MenuItem item4 = new MenuItem("menu item 4");
-		menuBar.add(menu);
-		menu.add(item1);
+		Menu menu = new Menu("file"); 
+		MenuItem item1 = new MenuItem("save");
+		MenuItem item2 = new MenuItem("load");
+		Menu menu1 = new Menu("game"); 
+		MenuItem item3 = new MenuItem("run");
+		MenuItem item4 = new MenuItem("new game");
+		
+		// the "save" action
 		item1.addActionListener(new ActionListener() {
 
 			@Override
@@ -61,10 +60,24 @@ public class MainWindow extends JFrame implements MouseListener
 			}
 			
 		});
+		// the "new game" action
+		item4.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+			System.out.println("start new game");
+			game.clear();
+			repaint();
+			}
+			
+		});
+		menu.add(item1);
 		menu.add(item2);
-		menuBar.add(menu1);
 		menu1.add(item3);
 		menu1.add(item4);
+		menuBar.add(menu);
+		menuBar.add(menu1);
+
 		this.setMenuBar(menuBar);
 		
 		try {
@@ -74,52 +87,42 @@ public class MainWindow extends JFrame implements MouseListener
 		}		
 	}
 
+	
+	
+	
 	int x = -1;
 	int y = -1;
-    int z=-1;
-    int q=-1;
+	
 	public void paint(Graphics g)
 	{
 		ArrayList<Packman>p=game.getPackmans();
 		ArrayList<Fruit>f=game.getFruits();
 		g.drawImage(myImage, 0, 0, this);
+		
+		 // print the Packmans
 		 Iterator<Packman> it1 =p.iterator();
 		 Packman temp_Packman ;
-			// run over all the layer
 			 while(it1.hasNext()) {
 				 temp_Packman=(Packman)it1.next();
-				 g.fillOval((int)temp_Packman.Getpoint().x(), (int)temp_Packman.Getpoint().y(), 10, 10);
-				 //g.setFont(getFont());
+				 g.fillOval((int)temp_Packman.Getpoint().x(), (int)temp_Packman.Getpoint().y(), 20, 20);
 			 }
+			 
+			 // print the fruits
 			 Iterator<Fruit> it2 =f.iterator();
 			 Fruit temp_Fruit ;
 				// run over all the layer
 				 while(it2.hasNext()) {
 					 temp_Fruit=(Fruit)it2.next();
-					 g.fillOval((int)temp_Fruit.Getpoint().x(),(int) temp_Fruit.Getpoint().y(), 20, 20);
+					 g.fillOval((int)temp_Fruit.Getpoint().x(),(int) temp_Fruit.Getpoint().y(), 10, 10);
 					 g.setFont(getFont());
 				 }
 	
-//		if(x!=-1 && y!=-1)
-//		{
-//			int r = 10;
-//			x = x - (r / 2);
-//			y = y - (r / 2);
-//			g.fillOval(x, y, r, r);
-//		}
-//		if(z!=-1 && q!=-1)
-//		{
-//			int p = 20;
-//			z = z - (p / 2);
-//			q = q - (p / 2);
-//			g.fillOval(z, q, p, p);
-//		}
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent arg) {
-		//if right click
-		if(arg.getButton()==3) {
+		//if right click add Packman
+		if(arg.getButton()==1) {
 		System.out.println("mouse Clicked");
 		System.out.println("("+ arg.getX() + "," + arg.getY() +")");
 		x = arg.getX();
@@ -128,8 +131,8 @@ public class MainWindow extends JFrame implements MouseListener
 		game.addPac(p1);
 		repaint();
 		}
-		//if left click
-		if(arg.getButton()==1) {
+		//if left click add Fruit
+		if(arg.getButton()==3) {
 			
 			x = arg.getX();
 			y = arg.getY();
